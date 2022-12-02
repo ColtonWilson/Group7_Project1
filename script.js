0404bdf37a3c4e210f81c407a51f966bb37d0386
Colton Wilson <colton_wilson93@yahoo.com>
2022-12-01 19:22:24 -0800

    Added Both API's and verified they are working in the console

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Test");
+var baseUrlOne = "https://api.coinranking.com/v2/coins"
+var baseUrlTwo = "https://api.currencyfreaks.com/latest?apikey=74322d673c074013814dab12cbf6be53"
+var proxyUrl = "https://cors-anywhere.herokuapp.com/"
+var apiKeyOne = "coinranking8e254f87ce60317d0418cdb55cb43b2fce3d857bd5f356ec"
+
+//First API
+fetch(`${proxyUrl}${baseUrlOne}`, {
+    method: "GET",
+    headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': `${apiKeyOne}`,
+        'Access-Control-Allow-Origin': '*'
+    }
+}).then((response) => {
+    if(response.ok)
+    {
+        response.json().then((json)=> {
+            console.log(json.data.coins)
+
+           
+        })
+    }
+}).catch((error) => {
+    console.log(error);
+ });
+
+
+ //Second API
+ fetch(`${baseUrlTwo}`, {
+    
+}).then((response) => {
+    if(response.ok)
+    {
+        response.json().then((json)=> {
+            console.log(json)
+
+        })
+    }
+}).catch((error) => {
+    console.log(error);
+ });
